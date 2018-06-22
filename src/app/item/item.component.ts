db48b51ba720cd0121eb6c9cd17674d76f055766
roim1562 <r.orsi97@gmail.com>
2018-06-22 10:38:55 +0300

    Finish internationalization

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -25,8 +25,8 @@ export class ItemComponent implements OnInit {
   type: Type = null;
   setting: Setting = null;
   element: I18nElement = null;
-  arrayOfI18n: Array<I18nElement> = [];
-  i18n: Object;
+  languages: Array<I18nElement> = [];
+  languageObj: Object;
 
   itemId = 0;
   languageId = 0;
@@ -42,26 +42,16 @@ export class ItemComponent implements OnInit {
     this.slideshow = Slideshow.create();
     this.type = Type.create();
     this.setting = Setting.create();
-    this.i18n = Object.create(null);
+    this.languageObj = Object.create(null);
   }
   addNewLanguage() {
     this.languageId++;
     this.element = I18nElement.create({ languageName : '', resources : new Array<ResourceSample>(new ResourceSample({resourceName : '', resourceValue : ''}))});
-    this.arrayOfI18n.push(this.element);
+    this.languages.push(this.element);
   }
-  addNewResource = (i18n: I18nElement): any => {
-    i18n.resources.push( new ResourceSample( { resourceName : '', resourceValue : ''} ));
+  addNewResource = (languageObj: I18nElement): any => {
+    languageObj.resources.push( new ResourceSample( { resourceName : '', resourceValue : ''} ));
   }
-  //bindLanguageAndAddToMainObject: any = () => {
-
-    // for (let i = 0; i < this.arrayOfI18n.length; i++) {
-    //   const name = this.arrayOfI18n[i].languageName;
-    //   this.i18n[name] = {};
-    //   for (let j = 0 ; j < this.arrayOfI18n[i].resources.length ; j++) {
-    //     this.i18n[name][stringOrDefault(this.arrayOfI18n[i].resources[j].resourceName)] = this.arrayOfI18n[i].resources[j].resourceValue;
-    //   }
-    // }
-  //}
   addSlide(slide: Slide): void {
     this.slides.push(slide);
   }
@@ -72,28 +62,24 @@ export class ItemComponent implements OnInit {
   }
   adLink(link: LinksArray): void {
     this.links.push(link);
-    console.log('Link  : ', this.links);
   }
   addItem() {
     ++this.itemId;
     this.items.push(new Items({id : this.itemId}));
   }
-  /*pushObject(item: Items): void {
-    this.items.push(item);
-  }*/
   exportItems(format: string = 'json'): string {
 
-    for (let i = 0; i < this.arrayOfI18n.length; i++) {
-      const name = this.arrayOfI18n[i].languageName;
-      this.i18n[name] = {};
-      for (let j = 0 ; j < this.arrayOfI18n[i].resources.length ; j++) {
-        this.i18n[name][stringOrDefault(this.arrayOfI18n[i].resources[j].resourceName)] = this.arrayOfI18n[i].resources[j].resourceValue;
+    for (let i = 0; i < this.languages.length; i++) {
+      const name = this.languages[i].languageName;
+      this.languageObj[name] = {};
+      for (let j = 0 ; j < this.languages[i].resources.length ; j++) {
+        this.languageObj[name][stringOrDefault(this.languages[i].resources[j].resourceName)] = this.languages[i].resources[j].resourceValue;
       }
     }
 
     switch ((format || '').toLowerCase()) {
       case 'json':
-          return JSON.stringify({items: this.items, slideshow: this.slideshow, types: this.type, settings: this.setting, i18n: this.i18n});
+          return JSON.stringify({items: this.items, slideshow: this.slideshow, types: this.type, settings: this.setting, i18n: this.languageObj});
       default:
         console.warn(`Unknown export format'${format}'`);
         return this.toString();
@@ -102,28 +88,26 @@ export class ItemComponent implements OnInit {
   importItems(config: string, format: string = 'json'): boolean {
     switch (format.toLowerCase()) {
       case 'json':
-        this.items = JSON.parse(config).items;
-        this.slideshow = JSON.parse(config).slideshow;
-        this.type = JSON.parse(config).types;
-        this.setting = JSON.parse(config).settings;
-        this.i18n = JSON.parse(config).i18n;
 
-        console.log('Items : ', this.items);
-        console.log('Slideshow : ', this.slideshow);
-        console.log('Type : ', this.type);
-        console.log('Setting : ', this.setting);
-        console.log('i18n : ', this.i18n);
+        const json = JSON.parse(config);
 
-        for( const language in this.i18n) {
-            // console.log(this.i18n[key]);
-            this.arrayOfI18n[language] = I18nElement.create({languageName: language});
+        this.items = json.items;
+        this.slideshow = json.slideshow;
+        this.type = json.types;
+        this.setting = json.settings;
+        this.languageObj = json.i18n;
 
-            for( const resourceName in this.i18n[language]) {
-              this.arrayOfI18n[language].resources.push( new ResourceSample({'resourceName' : resourceName, 'resourceValue' : this.i18n[language][resourceName] }));
+        this.languages = [];
+
+        for( const language in this.languageObj) {
+            const element = I18nElement.create({languageName: language});
+            const resources = this.languageObj[language];
+
+            for( const resourceName in resources ) {
+              element.resources.push(new ResourceSample({'resourceName' : resourceName, 'resourceValue' : resources[resourceName] }));
             }
+            this.languages.push(element);
         }
-        console.log(this.arrayOfI18n);
-
         break;
       default:
         return false;
